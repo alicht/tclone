47075b074cb558b401b8f6fcace4635ef9a711e7
Avi Lichtschein <AviLichtschein@gmail.com>
2014-12-06 19:40:31 -0500

    added some initial info, generated Person Authority actions

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,20 @@
+// import org.grails.twitter.auth.*
 class BootStrap {
 
+	// def springSecurityService
+
     def init = { servletContext ->
+    	//if(!Person.count()) {
+    	//	def userRole = new Authority(authority: 'Role_User').save
+
+    	//	def password = springSecurityService.encodePassword('password')
+
+    	//	[avi: 'Avi Lichtschein', elie: 'Elie Lichtschein']
+
+    	//	def user = new Person(username: userName, realname: 'realName')
+    	//	PersonAuthority.create user, userRole, true
+    	// }
+
     }
     def destroy = {
     }
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -115,3 +115,20 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'org.grails.twitter.auth.Person'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'org.grails.twitter.auth.PersonAuthority'
+grails.plugin.springsecurity.authority.className = 'org.grails.twitter.auth.Authority'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                              ['permitAll'],
+	'/index':                         ['permitAll'],
+	'/index.gsp':                     ['permitAll'],
+	'/assets/**':                     ['permitAll'],
+	'/**/js/**':                      ['permitAll'],
+	'/**/css/**':                     ['permitAll'],
+	'/**/images/**':                  ['permitAll'],
+	'/**/favicon.ico':                ['permitAll']
+]
+
