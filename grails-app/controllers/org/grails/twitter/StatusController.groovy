32cec56acdf89e49150d38577f6065b81e645346
Avi Lichtschein <AviLichtschein@gmail.com>
2014-12-07 12:43:05 -0500

    index method and lookup person modified

--- grails-app/controllers/org/grails/twitter/StatusController.groovy
+++ grails-app/controllers/org/grails/twitter/StatusController.groovy
@@ -5,8 +5,12 @@ import org.grails.twitter.auth.Person
 class StatusController {
 
     def springSecurityervice
+    // will be injected into controller before the code executes
 
-    def index() { }
+    def index() { 
+    	def messages = currentUserTimeline()
+    	[messages: messages]
+    }
 
     def updateStatus = {
     	def status = new Status(message: params.message)
@@ -15,7 +19,20 @@ class StatusController {
     	def messages = currentUserTimeline()
     	render template: 'messages', collection: messages, var: 'message'
     }
+
+    private currentUserTimeline(){
+    	def per = lookupPerson()
+    	def messages = Status.withCriteria{
+    			author{
+    				eq 'username', per.username
+    			}
+       		maxResults 10
+    		order 'dateCreated', 'desc'
+    }
+    messages
+}
+
     private lookupPerson() {
-    Person.get(springSecurityervice.principal.id)
+    Person.get(springSecurityervice.principal.username)
 	}
 }
